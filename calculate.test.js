cb7da1ac64805b68fe0eef9e1c701a2b0c464c72
eliasfernandescout <eliasfernandescout@gmail.com>
2022-04-15 08:02:22 -0300

    add calculatetest

---
+++ calculate.test.js
@@ -0,0 +1,34 @@
+const calculate1 = require('./calculate1')
+
+test('Deve calcular o valor de uma corrida de taxi em dias normais', () => {
+    //given range
+    const distante = 1000;
+    const date = new Date('2021-07-10T10:00:00')
+    //when
+    const price = calculate1.calc(distante, date);
+    //then
+    expect(price).toBe(2100);
+    
+});
+
+test('Deve calcular o valor de uma corrida de taxi a noite', () => {
+    //given range
+    const distante = 1000;
+    const date = new Date('2021-07-10T23:00:00')
+    //when
+    const price = calculate1.calc(distante, date);
+    //then
+    expect(price).toBe(3900);
+    
+});
+
+test('Deve calcular o valor de uma corrida de taxi no domingo', () => {
+    //given range
+    const distante = 1000;
+    const date = new Date('2021-07-11T23:00:00')
+    //when
+    const price = calculate1.calc(distante, date);
+    //then
+    expect(price).toBe(3900);
+    
+});
